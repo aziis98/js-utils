1aafb792a10186cc4a8be90bda4bc4345ae30769
aziis98 <antonio.delucreziis@gmail.com>
2017-01-03 14:12:40 +0100

    Added the compileTemplateFunction and a readme with some common CDNs

---
+++ README.md
@@ -0,0 +1,9 @@
+# Common CDNs
+
+```html
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.2/lodash.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js"></script>
+
+```
--- templates.js
+++ templates.js
@@ -1,9 +1,21 @@
 String.prototype.format = function() {
     const args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
+    return this.replace(/{(\d+)}/g, function(match, number) {
 	return typeof args[number] != 'undefined'
             ? args[number]
             : match
 	;
     });
+};
+
+exports.compileTemplate = function compileTemplate(source, data) {
+	data = data || {};
+
+	const compiledTemplate = source.replace(/\{\{(.+?)\}\}/g, function(match, code) {
+		with(data) {
+			return eval(code);
+		}
+	});
+
+	return $.parseHTML(compiledTemplate.trim())[0];
 };
